4011efbfeed51355c50cd20d954224d7f48f6bfd
Otavio Guerra <otaviomguerra@hotmail.com>
2021-01-24 14:00:15 -0300

    Implemented base distortion classification code

---
+++ backend/backend_utils.py
@@ -0,0 +1,20 @@
+DISTORTION_LABELS = {
+    '01': 'Additive Gaussian noise',
+    '02': 'Additive noise',
+    '03': 'Spatially correlated noise',
+    '04': 'Masked noise',
+    '05': 'High frequency noise',
+    '06': 'Impulse noise',
+    '07': 'Quantization noise',
+    '08': 'Gaussian blur',
+    '09': 'Image denoising',
+    '10': 'JPEG compression',
+    '11': 'JPEG2000 compression',
+    '12': 'JPEG transmission errors',
+    '13': 'JPEG2000 transmission errors',
+    '14': 'Non eccentricity pattern noise',
+    '15': 'Local block-wise distortions of different intensity',
+    '16': 'Mean shift (intensity shift)',
+    '17': 'Contrast change'
+}
+
--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,14 @@
+import io
+
 import imquality.brisque as brisque
 from fastapi import FastAPI, File
 from fastapi.responses import PlainTextResponse
 from PIL import Image
-import io
 
 app = FastAPI(
     title="Quality score and 'problem' classification ",
-    description="""Obtain a quality score for any given image and if it has a problem
-    classifies that problem.""",
+    description="""Obtain a quality score for any given image and if it has a distortion
+    classifies that distortion.""",
     version="0.1.0",
 )
 
@@ -15,7 +16,7 @@ app = FastAPI(
 @app.post("/brisque-score", response_class=PlainTextResponse)
 def get_brisque_score(file: bytes = File(...)):
     """Commputes the brisque score for a 
-    given image
+    given image.
 
     Parameters
     ----------
@@ -30,5 +31,21 @@ def get_brisque_score(file: bytes = File(...)):
     input_image = Image.open(io.BytesIO(file)).convert("RGB")
 
     print("BRISQUE SCORE: ", str(round(brisque.score(input_image), 4)))
-    
+
     return str(round(brisque.score(input_image), 4))
+
+@app.post("/classification")
+def get_distortion_class(file: bytes = File(...)):
+    """Classifies image in 
+
+    Parameters
+    ----------
+    file : bytes, optional
+        Image file object, by default File(...)
+
+    Returns
+    -------
+    str
+        Distortion class
+    """
+    return NotImplemented
